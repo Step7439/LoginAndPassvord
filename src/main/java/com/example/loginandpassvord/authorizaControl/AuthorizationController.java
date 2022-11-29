a650ebd568d638b2909b4b2c97b9bbc3bb7aabff
Step7439 <zamashikovs@mail.ru>
2022-11-29 22:31:15 +0900

    Add Dockerfile

--- src/main/java/com/example/loginandpassvord/authorizaControl/AuthorizationController.java
+++ src/main/java/com/example/loginandpassvord/authorizaControl/AuthorizationController.java
@@ -2,24 +2,25 @@ package com.example.loginandpassvord.authorizaControl;
 
 import com.example.loginandpassvord.authoritiesModel.Authorities;
 import com.example.loginandpassvord.authorizaService.AuthorizationService;
-import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 
 @RestController
-public class AuthorizationController {
+public class AuthorizationController implements WebMvcConfigurer {
     AuthorizationService service;
 
-    public AuthorizationController (AuthorizationService service) {
+    public AuthorizationController(AuthorizationService service) {
         this.service = service;
     }
 
 
     @GetMapping("/authorize")
-    public List<Authorities> getAuthorities(@RequestParam ("user") @Validated String user, @RequestParam ("password") @Validated String password) {
+    public List<Authorities> getAuthorities(@RequestParam("user") @Validated String user, @RequestParam("password") @Validated String password) {
         return service.getAuthorities(user, password);
     }
 }
